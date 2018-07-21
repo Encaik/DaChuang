a963d24609005ec1469ebe10cecbbd0afa0fb426
st843391622 <843391622@qq.com>
2018-07-21 11:59:04 +0800

    7.21

--- index/search.py
+++ index/search.py
@@ -4,7 +4,7 @@ from . import pdftext
 
 # 关键词查找并返回结果字符串
 def parse(str):
-    name = '1204734306'
+    name = '1204740521'
     pdftext.gettext(name)
     fp = open('static/data/'+name+'.txt', 'rt')
     inner = fp.read()
